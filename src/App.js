8cc9444268a9502fb39f10d5f509347b40993cd2
AbubakarShf <ItsAbubakarShf@gmail.com>
2021-05-07 11:50:34 +0500

    structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>App</h1>
+            <h3>Hello</h3>
+        </div>
+    );
+}
+
+export default App;
