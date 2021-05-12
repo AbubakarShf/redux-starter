e2a4d6da88d02378b533fe07e721fc9ebe48cb51
AbubakarShf <ItsAbubakarShf@gmail.com>
2021-05-12 14:19:59 +0500

    Final-imp-done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import "./App.css";
-import Increment from "./redux/actions/counter.action.js";
+import React from "react";
+// import Increment from "./redux/actions/counter.action.js";
 import { useSelector, useDispatch } from "react-redux";
 function App() {
     const counter = useSelector((store) => store.counter);
     const trigger = useDispatch();
     return (
-        <div className="App">
-            <h1 className="App-Name">CounterApp</h1>
-            <h3 className="Current-State">Counter: {counter}</h3>
-            <button onClick={() => trigger(Increment)} className="Inc-btn">
-                Increment
-            </button>
-        </div>
+        <React.Fragment>
+            <div className="App">
+                <h1 className="App-Name">CounterApp</h1>
+                <h3 className="Current-State">Counter: {counter}</h3>
+                <div className="btn-group">
+                    <button
+                        onClick={() => trigger({ type: "INCREMENT" })}
+                        className="Inc-btn"
+                    >
+                        Increment
+                    </button>
+                    <button
+                        onClick={() => trigger({ type: "DECREMENT" })}
+                        className="Dec-btn"
+                    >
+                        Decrement
+                    </button>
+                </div>
+            </div>
+        </React.Fragment>
     );
 }
 
