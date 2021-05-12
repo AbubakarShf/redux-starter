3245466f89466c1b27f52bde192cd929250a62b3
AbubakarShf <ItsAbubakarShf@gmail.com>
2021-05-12 14:32:01 +0500

    Updates

---
+++ precache-manifest.3ac9a9a2e620b4a5da33d56d3bcd94fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1627798c1e2db19ef488",
+    "url": "/redux-starter/static/css/main.17f18d73.chunk.css"
+  },
+  {
+    "revision": "1627798c1e2db19ef488",
+    "url": "/redux-starter/static/js/main.b318ae23.chunk.js"
+  },
+  {
+    "revision": "d9417109caa3d28b7019",
+    "url": "/redux-starter/static/js/runtime~main.7c13f6a7.js"
+  },
+  {
+    "revision": "c46ac91aba0943197239",
+    "url": "/redux-starter/static/js/2.4a2e7e60.chunk.js"
+  },
+  {
+    "revision": "03c3eaa6dba72a240be5f3aed5f103e9",
+    "url": "/redux-starter/index.html"
+  }
+];
